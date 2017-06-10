370bc055d21f1f2b1afc22b65de4b4d7a73b51bd
daniel-clouse <daniel-clouse@users.noreply.github.com>
2017-06-10 15:50:47 -0700

    Now drawing bounding boxes from tif tags

--- dash696.py
+++ dash696.py
@@ -3,18 +3,9 @@ import urllib
 import numpy as np
 import struct
 
-class Bbox:
-    def __init__(self, x0=0, y0=0, x1=0, y1=0):
-        self.x0 = x0
-        self.y0 = y0
-        self.x1 = x1
-        self.y1 = y1
-
 def parse_tif_tags(jpg):
     TIFOFF = 12
-    #struct.unpack('=cccHccccccc')
     mm, version, ifd_offset, ifd_count = struct.unpack('!HHIH', jpg[TIFOFF:TIFOFF+10])
-    print(str(ifd_offset) + " " + str(ifd_count))
     columns = 0
     rows = 0
     bbox_offset = 0
@@ -22,8 +13,6 @@ def parse_tif_tags(jpg):
     for ii in range(0, ifd_count):
         start_off = TIFOFF + ifd_offset + 2 + ii * 12
         tag, type, count, num = struct.unpack('!HHII', jpg[start_off:start_off + 12])
-        print("ttcn " + str(tag) + " " + str(type) + " " + str(count) + " " + str(num))
-
         if tag == 0x0100:
             columns = num
         elif tag == 0x0101:
@@ -31,14 +20,13 @@ def parse_tif_tags(jpg):
         elif tag == 0x9696:
             bbox_count = count
             bbox_offset = num
-    print("crb = " + str(columns) + " " + str(rows) + " " + str(bbox_count) + " " + str(bbox_offset))
-    bbox_list = []
+    rect_list = []
     if bbox_offset > 0:
         for ii in range(0, bbox_count / 4):
             start_off = TIFOFF + bbox_offset + ii * 8
             x0, y0, x1, y1 = struct.unpack('!HHHH', jpg[start_off:start_off + 8])
-            bbox_list.append(Bbox(x0, y0, x1, y1))
-            print("bbox " + str(x0) + " " + str(y0) + " " + str(x1) + " " + str(y1))
+            rect_list.append(((x0, y0), (x1, y1)))
+    return (columns, rows, rect_list)
 
 
 
@@ -51,9 +39,11 @@ while True:
     b = bytes.find('\xff\xd9')
     if a != -1 and b != -1:
         jpg = bytes[a:b+2]
-        parse_tif_tags(jpg)
+        cols, rows, rect_list = parse_tif_tags(jpg)
         bytes = bytes[b+2:]
         i = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8), cv2.CV_LOAD_IMAGE_COLOR)
+        for rect in rect_list:
+            cv2.rectangle(i, rect[0], rect[1], (0,0,255))
         cv2.imshow('i', i)
         if cv2.waitKey(1) == 27:
             exit(0)
